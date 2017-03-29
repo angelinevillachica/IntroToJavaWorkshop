5c8057f159aaa7dbcbe083325466dd8ec6bfa78f
angeline <league@WorthyMini.local>
2017-03-29 12:05:00 -0700

    we made computers speak

---
+++ src/Greeter.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+System.out.println("when in doubt go to the libary");
+JOptionPane.showMessageDialog(null, "hello my name is coffe bean");
+JOptionPane.showMessageDialog(null, "you look very nice today");
+JOptionPane.showMessageDialog(null,"you are amazing");
+JOptionPane.showMessageDialog(null,"you are great at what you do");
+JOptionPane.showMessageDialog(null,"your hair looks nice");
+JOptionPane.showMessageDialog(null, "it's time for me to go. bye");
+}
+
+}
---
+++ src/snooper.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+
+public class snooper {
+public static void main(String[] args) {
+String name =JOptionPane.showInputDialog("what is your name?");
+JOptionPane.showMessageDialog(null,"hello "+ name + " my name is amber");
+String howtheyare =JOptionPane.showInputDialog("how are you "+ name);
+String phonenumber =JOptionPane.showInputDialog("lets hangout.give me your phone number so i can call you "+ name);
+String adress =JOptionPane.showInputDialog("what is your adress so i can visit you " + name);
+String creditnumber =JOptionPane.showInputDialog("what is your credit card number");
+JOptionPane.showInputDialog("thank you for your personal information bye expect a charge on your bank account");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}
